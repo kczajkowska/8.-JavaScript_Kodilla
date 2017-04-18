610e34c465dd9e96133d50f977ce12a2f2b307b4
Kasia <katarzyna.czajkowskaa@gmail.com>
2017-04-18 13:14:53 +0200

    8.4.Typy danych, po co Ci te zmienne. Zadanie: Pole trójkąta

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = 2,
+	h = 4,
+	traingleArea;
+
+traingleArea =prompt(a*h/2);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
