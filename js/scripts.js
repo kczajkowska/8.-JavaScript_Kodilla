71b09954c65d1ca4ca6d7f4c02a7659da4267aea
Kasia <katarzyna.czajkowskaa@gmail.com>
2017-04-26 13:24:13 +0200

    8.6. Funkcje Zadanie: Tworzymy funkcje

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(11,15),
+	triangle3Area = getTriangleArea(0,5);
+
+
+function getTriangleArea(a, h) {
+	if (a > 0 == h > 0) {
+		return (a*h/2)
+	}
+	else {
+		console.log('Nieprawidłowe dane')
+	}
+}
+
+console.log( 'Pole trójkąta 1 ' + triangle1Area );
+console.log( 'Pole trójkąta 2 ' + triangle2Area );
+console.log( 'Pole trójkąta 3 ' + triangle3Area );
