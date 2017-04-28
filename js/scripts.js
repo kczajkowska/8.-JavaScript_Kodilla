f3c8ae311e46fd8d23e6847f98d92748a2787000
Kasia <katarzyna.czajkowskaa@gmail.com>
2017-04-28 22:22:35 +0200

    8.8 Łańcuchy znaków Zadanie: Papugi i słonie

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var txt = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animalUpperCased = animal.toUpperCase();
+
+console.log(animalUpperCased);
+
+var txtCharsAfter = txt.replace ('Papugi', animal);
+console.log(txtCharsAfter);
+
+console.log(txtCharsAfter.length/2); 
