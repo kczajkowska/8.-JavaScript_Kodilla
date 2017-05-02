d58d25b93f9ee3cddff9d0f29d752cf8290c289a
Kasia <katarzyna.czajkowskaa@gmail.com>
2017-05-02 13:37:21 +0200

    8.9. Pętle Zadanie: Choinka

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var n = prompt ('Podaj wysokość choinki');
+
+function drawTree(n) {
+	for ( var i = 1 ; i <= n; i++) {
+		var star = '*';
+			for ( var j = 1 ; j <= i-1; j++) {
+			star += '*';
+			}
+		console.log(star);
+		}
+
+}
+
+drawTree(n);
